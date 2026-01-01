3f56fb4647ac4657e27d9c6aa11e24273eec757a
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2026-01-01 13:33:14 -0300

    feat: Importação do MainContentComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./components/header/header.component";
+import { HeaderComponent } from './components/header/header.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, MainContentComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'projeto-go-task';
