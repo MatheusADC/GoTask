80648f9e42b8a0fc6e8e3a9a0d90f88a7a70064e
Matheus Amaral <161625293+MatheusADC@users.noreply.github.com>
2026-03-31 07:20:12 -0300

    ✨ feat: Atualização do status da tarefa no service

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -4,13 +4,14 @@ import { ITask } from "../interfaces/task.interface";
 import { ITaskFormControls } from "../interfaces/task-form-controls.interface";
 import { TaskStatusEnum } from "../enums/task-status.enum";
 import { generateUniqueIdWithTimestamp } from "../utils/generate-unique-id-with-timestamp";
+import { TaskStatus } from "../types/task-status";
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
   // Tarefas em A fazer
-  private todoTasks$ =  new BehaviorSubject<ITask[]>([]);
+  private todoTasks$ = new BehaviorSubject<ITask[]>([]);
   readonly todoTasks = this.todoTasks$
     .asObservable()
     .pipe(map((tasks) => structuredClone(tasks)));
@@ -38,5 +39,39 @@ export class TaskService {
     const currentList = this.todoTasks$.value;
 
     this.todoTasks$.next([...currentList, newTask]);
+  }
+
+  updateTaskStatus(
+    taskId: string,
+    taskCurrentStatus: TaskStatus,
+    taskNextStatus: TaskStatus,
+  ) {
+    const currentTaskList = this.getTaskListByStatus(taskCurrentStatus);
+    const nextTaskList = this.getTaskListByStatus(taskNextStatus);
+    const currentTask = currentTaskList.value.find(
+      (task) => task.id === taskId,
+    );
+
+    if (currentTask) {
+      // Atualiza o status da tarefa
+      currentTask.status = taskNextStatus;
+      // Remove a tarefa da lista atual
+      const currentTaskListWithoutTask = currentTaskList.value.filter(
+        (task) => task.id !== taskId,
+      );
+      currentTaskList.next([...currentTaskListWithoutTask]);
+
+      // Adiciona a tarefa na nova lista
+      nextTaskList.next([...nextTaskList.value, { ...currentTask }]);
+    }
+  }
+
+  private getTaskListByStatus(TaskStatus: TaskStatus) {
+    const taskListObj = {
+      [TaskStatusEnum.TODO]: this.todoTasks$,
+      [TaskStatusEnum.DOING]: this.doingTasks$,
+      [TaskStatusEnum.DONE]: this.doneTasks$,
+    };
+    return taskListObj[TaskStatus];
   }
 }
