90d64f85534cc9f1a53380d6162bb79425ffc537
Matheus Amaral <161625293+MatheusADC@users.noreply.github.com>
2026-04-11 17:18:07 -0300

    ✨ feat: Adição do foco no input de comentário com ViewChild

--- src/app/components/task-comments-modal/task-comments-modal.component.ts
+++ src/app/components/task-comments-modal/task-comments-modal.component.ts
@@ -1,6 +1,6 @@
 import { generateUniqueIdWithTimestamp } from './../../utils/generate-unique-id-with-timestamp';
 import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IComment } from '../../interfaces/comment.interface';
 import { ITask } from '../../interfaces/task.interface';
@@ -14,6 +14,8 @@ import { ITask } from '../../interfaces/task.interface';
 export class TaskCommentsModalComponent {
   taskCommentsChanged = false;
   commentControl = new FormControl('', [Validators.required]);
+
+  @ViewChild('commentInput') commentInputRef!: ElementRef<HTMLInputElement>;
 
   readonly _task: ITask = inject(DIALOG_DATA);
   readonly _dialogRef: DialogRef<boolean> = inject(DialogRef);
@@ -35,6 +37,9 @@ export class TaskCommentsModalComponent {
 
     // Atualizar a flag/prop se houve a alteração nos comentários
     this.taskCommentsChanged = true;
+
+    // Focando no elemento de input
+    this.commentInputRef.nativeElement.focus();
   }
 
   onCloseModal() {
