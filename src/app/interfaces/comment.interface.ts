1737aa475540d999e6c1a7164f2bce7951514ce6
Matheus Amaral <161625293+MatheusADC@users.noreply.github.com>
2026-06-04 16:31:28 -0300

    🧹 cleanup: Código desnecessário

--- src/app/interfaces/comment.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface IComment {
-  id: string;
-  description: string;
-}
--- src/app/interfaces/task-form-modal-data.interface.ts
+++
@@ -1,6 +0,0 @@
-import { ITaskFormControls } from "./task-form-controls.interface";
-
-export interface ITaskFormModalData {
-  mode: 'create' | 'edit';
-  formValues: ITaskFormControls;
-}
